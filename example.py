b96aa2f56c26e0e7c4ffcc4d90d0bac9125ec524
Emanuel Kozerski <emanuelkoz.97@hotmail.com>
2025-02-03 22:49:43 -0300

    Removing skinning data from example.py to keep it simple.

--- example.py
+++ example.py
@@ -1,10 +1,9 @@
 import ctypes
-
-from api_data_types import _Transform, HASH, Float2D
-from components import Camera, Float3D, Vertex, MeshSurface, Mesh, MeshInstance, Transform, SphereLight, SkinningData, \
-    Skeleton, OpacityPBR
-from core import RTXRemixAPI, StartupInfo
 import tkinter as tk
+
+from api_data_types import HASH, Float2D
+from components import Camera, Float3D, Vertex, MeshSurface, Mesh, MeshInstance, Transform, SphereLight, OpacityPBR
+from core import RTXRemixAPI, StartupInfo
 
 
 def main():
@@ -18,12 +17,12 @@ def main():
     startup_info = StartupInfo(hwnd=window.winfo_id())
     remix_api.init(startup_info)
 
-    mat = OpacityPBR(mat_hash=HASH(0x1), albedo_texture="test.dds")
-    return_code = remix_api.create_material(mat)
+    mat = OpacityPBR(mat_hash=HASH(0x1))
+    remix_api.create_material(mat)
 
     # Creating a Camera
     camera = Camera(
-         position=Float3D(0, 0, 0), forward=Float3D(0, 0, 1), up=Float3D(0, 1, 0), right=Float3D(1, 0, 0), fov_y=70,
+         position=Float3D(0, 0.3, 0), forward=Float3D(0, 0, 1), up=Float3D(0, 1, 0), right=Float3D(1, 0, 0), fov_y=70,
          aspect=float(window_width)/window_height, near_plane=0.1, far_plane=1000
     )
     # Creating a Mesh Asset and a MeshInstance
@@ -32,35 +31,16 @@ def main():
         Vertex(position=Float3D(0, 5, 10), normal=Float3D(0, 0, -1), texcoord=Float2D(0,1), color=0xFFFFFF).as_struct(),
         Vertex(position=Float3D(-5, -5, 10), normal=Float3D(0, 0, -1), texcoord=Float2D(1,0)).as_struct()
     ]
-    skinning_data = SkinningData(
-        bones_per_vertex=2,
-        blend_weights=[0.0, 0.5, 1.0,    1.0, 0.5, 0.0],  # 3 vertices, 2 bones_per_vertex
-        blend_indices=[0, 1, 0, 1, 0, 1],  # 3 vertices, 2 bones_per_vertex
-    )
-    surface = MeshSurface(vertices=vertices, indices=[0, 1, 2], skinning_data=skinning_data, material=mat)
+    surface = MeshSurface(vertices=vertices, indices=[0, 1, 2], material=mat)
     mesh = Mesh(surfaces=[surface.as_struct()])
     remix_api.create_mesh(mesh)
-    skeleton = Skeleton(bone_count=2)
-    bone_transform1 = Transform(matrix=[
-        [1, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, 0, 1, 0],
-    ])
-    bone_transform2 = Transform(matrix=[
-        [1, 0, 0, 0],
-        [0, 1, 0, 3],
-        [0, 0, 1, 0],
-    ])
-    transform_array = (_Transform * 2)()
-    transform_array[0] = bone_transform1.as_struct()
-    transform_array[1] = bone_transform2.as_struct()
-    skeleton.set_bone_transforms(ctypes.byref(transform_array))
+
     mesh_instance = MeshInstance(
-        mesh=mesh, category_flags=ctypes.c_uint32(0), transform=Transform(), double_sided=1, skeleton=skeleton
+        mesh=mesh, category_flags=ctypes.c_uint32(0), transform=Transform(), double_sided=1
     )
 
     # Creating a Sphere Light
-    light = SphereLight(position=Float3D(0, 8, 0), radius=0.1, light_hash=ctypes.c_uint64(0x3), radiance=Float3D(100, 200, 100))
+    light = SphereLight(position=Float3D(3, 8, -5), radius=0.1, light_hash=HASH(0x3), radiance=Float3D(100, 200, 100))
     remix_api.create_light(light)
 
     window.update_idletasks()
@@ -76,9 +56,12 @@ def main():
         window.update_idletasks()
         window.update()
 
-    ret = remix_api.destroy_material(mat)
+    remix_api.destroy_material(mat)
+    remix_api.destroy_mesh(mesh)
+    remix_api.destroy_light(light)
     window.destroy()
     remix_api.shutdown()
+
 
 if __name__ == '__main__':
     main()
